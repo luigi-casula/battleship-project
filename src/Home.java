8f47f7ccee2fcbbb41f260c5c30c63230cd73edb
LOwOrenzo <lorenzocaboni05@gmail.com>
2023-04-29 17:31:58 +0200

    Playing Frame - WIP

--- src/Home.java
+++ src/Home.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.IOException;
 
 public class Home extends JFrame implements ActionListener, WindowListener {
     JLabel title_l = new JLabel("Battleship"); //titolo
@@ -66,7 +67,11 @@ public class Home extends JFrame implements ActionListener, WindowListener {
     public void actionPerformed(ActionEvent actionEvent) {
         if (actionEvent.getSource() == newgame_b) {
             //new NewGame();
-            new PlayingFrame();
+            try {
+                new PlayingFrame();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             dispose();
         }
     }
--- src/PlayingFrame.java
+++ src/PlayingFrame.java
@@ -1,6 +1,45 @@
-public class PlayingFrame {
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
-    public PlayingFrame() {
+public class PlayingFrame extends JFrame {
 
+
+    public PlayingFrame() throws IOException {
+        super("Battleship - Playing");
+        customizeFrame();
+    }
+
+    public void customizeFrame() throws IOException{
+        Container c = getContentPane(); //container
+        c.setLayout(new FlowLayout()); //definizione del layout manager
+
+        int width1 = 234;
+        int height1 = 84;
+        BufferedImage ship1img = ImageIO.read(new File("src/images/ship model 1.png"));
+        Image scaledImage1 = ship1img.getScaledInstance(width1, height1, Image.SCALE_SMOOTH);
+        JLabel ship1 = new JLabel(new ImageIcon(scaledImage1));
+        c.add(ship1);
+
+        int width2 = 274;
+        int height2 = 70;
+        BufferedImage ship2img = ImageIO.read(new File("src/images/ship model 2.png"));
+        Image scaledImage2 = ship2img.getScaledInstance(width2, height2, Image.SCALE_SMOOTH);
+        JLabel ship2 = new JLabel(new ImageIcon(scaledImage2));
+        c.add(ship2);
+
+        /*int width1 = 234;
+        int height1 = 84;
+        BufferedImage ship1img = ImageIO.read(new File("src/images/ship model 1.png"));
+        Image scaledImage1 = ship1img.getScaledInstance(width1, height1, Image.SCALE_SMOOTH);
+        JLabel ship1 = new JLabel(new ImageIcon(scaledImage1));
+        c.add(ship1);*/
+
+
+        setVisible(true);
+        setBounds(100,100, 300, 300);
     }
 }
